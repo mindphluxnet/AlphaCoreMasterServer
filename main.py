b3af4b534ef48d0d449cef77eeab7fea68bfb84a
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-04-13 13:28:58 +0200

    Better comments

--- main.py
+++ main.py
@@ -8,25 +8,18 @@ count_by_guid = {}
 
 @app.route('/checkin/<guid>/<server_id>')
 def checkin(guid, server_id):
-    """
-    Adds a new GUID to the dictionary with a count of 0 and the server ID it checked in from.
-    """
+    # Used at server start.
     count_by_guid[guid] = {'count': 0, 'server_id': server_id, 'last_updated': time.time()}
 
 @app.route('/update/<guid>/<count>')
 def update(guid, count):
-    """
-    Updates the count for a given GUID.
-    """
+    # Used every time an user logs into the world server.
     count_by_guid[guid]['count'] = count
     count_by_guid[guid]['last_updated'] = time.time()
 
 @app.route('/query')
 def query():
-    """
-    Queries the dictionary for all GUIDs and their associated counts. If a GUID has not been updated in the last 5 minutes,
-    its count is reset to 0.
-    """
+    # If the server has not updated its user count in the last 5 minutes reset it to zero.    
     for guid in count_by_guid:
         if time.time() - count_by_guid[guid]['last_updated'] >= 300:
             count_by_guid[guid]['count'] = 0
@@ -35,9 +28,7 @@ def query():
 
 @app.route('/checkout/<guid>')
 def checkout(guid):
-    """
-    Resets the count for a given GUID to 0.
-    """
+    # Used at server shutdown.
     count_by_guid[guid]['count'] = 0
     count_by_guid[guid]['last_updated'] = time.time()
 
