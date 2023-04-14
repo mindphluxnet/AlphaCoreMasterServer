bcc35ded451e44208fbb860b6f146da9ba6e5f31
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-04-14 10:36:05 +0200

    Simplify things a bit

--- main.py
+++ main.py
@@ -4,33 +4,34 @@ import time
 
 app = Flask(__name__)
 
-count_by_guid = {}
+count_by_server = {}
 
-@app.route('/checkin/<guid>/<server_id>')
-def checkin(guid, server_id):
-    # Used at server start.
-    count_by_guid[guid] = {'count': 0, 'server_id': server_id, 'last_updated': time.time()}
-
-@app.route('/update/<guid>/<count>')
-def update(guid, count):
-    # Used every time an user logs into the world server.
-    count_by_guid[guid]['count'] = count
-    count_by_guid[guid]['last_updated'] = time.time()
+@app.route('/report/<server_id>/<count>')
+def update(server_id, count):    
+    # Server reports player count once a minute.
+    if server_id not in count_by_server:
+        count_by_server[server_id] = {'count': count, 'last_updated': time.time()}
+    else:
+        count_by_server[server_id]['count'] = count
+        count_by_server[server_id]['last_updated'] = time.time()
 
 @app.route('/query')
 def query():
-    # If the server has not updated its user count in the last 5 minutes reset it to zero.    
-    for guid in count_by_guid:
-        if time.time() - count_by_guid[guid]['last_updated'] >= 300:
-            count_by_guid[guid]['count'] = 0
-            count_by_guid[guid]['last_updated'] = time.time()
-    return jsonify(count_by_guid)
+    # If a server has not updated its user count in the last 5 minutes reset it to zero.    
+    for server_id in count_by_server:
+        if time.time() - count_by_server[server_id]['last_updated'] >= 300:
+            count_by_server[server_id]['count'] = 0
+            count_by_server[server_id]['last_updated'] = time.time()
+    return jsonify(count_by_server)
 
-@app.route('/checkout/<guid>')
-def checkout(guid):
+@app.route('/checkout/<server_id>')
+def checkout(server_id):
     # Used at server shutdown.
-    count_by_guid[guid]['count'] = 0
-    count_by_guid[guid]['last_updated'] = time.time()
+    if server_id not in count_by_server:
+        count_by_server[server_id] = {'count': 0, 'last_updated': time.time()}
+    else:
+        count_by_server[server_id]['count'] = 0
+        count_by_server[server_id]['last_updated'] = time.time()
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8000)
